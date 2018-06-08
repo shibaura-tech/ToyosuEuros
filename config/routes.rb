ff65dc19ae6a1577a07d39dc880c15cf41c97034
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-08 13:43:02 +0900

    rootにログイン画面がくるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
+    root to: 'devise/sessions#new'
+    
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
