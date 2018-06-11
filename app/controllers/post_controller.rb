4ec21cc754b321a1d81ef39852240a4b7c32b7d3
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-12 02:54:30 +0900

    掲示板機能を一式追加

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,13 @@
 class PostController < ApplicationController
+  def create
+    @post = Post.new(params[:post].permit(:topic_id, :post_message))
+    @post.save
+    redirect_to topics_show_path(params[:post]['topic_id'])
+  end
+
+  def delete
+    @post = Post.find(params[:id])
+    @post.delete
+    redirect_to topics_index_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   get 'topics/index'
-  get 'topics/show'
+
+  get 'topics/show/:id' => 'topics#show', as: :topics_show
+
+  post 'topics/create' => 'topics#create'
+
+  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
+
+  post 'posts/create' => 'post#create', as: :post_create
+
+  delete 'posts/delete/:id' => 'post#delete', as: :post_delete
+
+
+
   root 'home#index'
 
   devise_for :users
