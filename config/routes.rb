8ede20ee2a1c6c483da1bca2393807f71a8e7953
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-12 00:25:44 +0900

    ログイン後homeのindexに

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
+
   devise_for :users
   devise_scope :user do
-    root to: 'devise/sessions#new'
-    
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
