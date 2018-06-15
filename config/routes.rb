20d389c95d398bf6a12ed23bc08d2fd5b7e99f07
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-15 23:51:28 +0900

    順序の整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
   get 'topics/index'
-
   get 'topics/show/:id' => 'topics#show', as: :topics_show
-
   post 'topics/create' => 'topics#create'
-
   delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
 
   post 'posts/create' => 'post#create', as: :post_create
-
   delete 'posts/delete/:id' => 'post#delete', as: :post_delete
-
-
-
-  root 'home#index'
 
   devise_for :users
   devise_scope :user do
