0a177324b71deb3b262c63138b9766853bad3cca
Doppon <zaxpkuraudo7777@gmail.com>
2018-06-18 11:04:09 +0900

    マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_172534) do
+ActiveRecord::Schema.define(version: 2018_06_18_020152) do
 
   create_table "posts", force: :cascade do |t|
     t.string "n"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_11_172534) do
     t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "post_user_name"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
